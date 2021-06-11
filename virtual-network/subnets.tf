bff909bfc7640b4ef65c45ab8d826f5174689a15
Jim Counts <jamesrcounts@outlook.com>
2021-06-11 11:17:23 -0400

    Fix subnet references

--- virtual-network/subnets.tf
+++ virtual-network/subnets.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subnet" "subnet" {
   for_each = var.subnets
 
-  address_prefixes     = [each.value]
+  address_prefixes     = [cidrsubnet(azurerm_virtual_network.vnet.address_space.0, each.value.0, each.value.1)]
   name                 = each.key
   resource_group_name  = var.resource_group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
