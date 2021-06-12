04971254bc80783818825d2244607262e09d941e
Jim Counts <jamesrcounts@outlook.com>
2021-06-12 17:36:59 +0000

    Cleanup

--- aks/variables.tf
+++ aks/variables.tf
@@ -1,8 +1,3 @@
-// variable "cert_secret_id" {
-//   description = "(Required) The secret id for the key vault secret containing the certificate an private key for this deployment."
-//   type        = string
-// }
-
 variable "environment" {
   description = "(Required) The environment tag, examples: dev, tst, prd."
   type        = string
@@ -15,16 +10,6 @@ variable "log_analytics_workspace" {
     name = string
   })
 }
-
-// variable "configuration_resource_group" {
-//   description = "(Required) The module will read configuration data from this resource group."
-//   type = object({
-//     id       = string
-//     name     = string
-//     location = string
-//     tags     = map(string)
-//   })
-// }
 
 variable "resource_group" {
   description = "(Required) The module will deploy resources into this resource group and copy tags from this resource group onto the resources."
