e413707885a76df362302de35800c27d01bbc412
Jim Counts <jamesrcounts@outlook.com>
2021-06-23 15:10:02 +0000

    make list

--- k8s-namespace/istio.tf
+++ k8s-namespace/istio.tf
@@ -21,14 +21,15 @@ locals {
   //     ]
   //   ])
 
-  kmanifests = flatten([
-    for key, path in local.manifests : [
-      for id in data.kustomization_overlay.istio_configuration[key].ids : {
-        id       = id
-        manifest = data.kustomization_overlay.istio_configuration[key].manifests[id]
-      }
-    ]
-  ])
+  kmanifests = tolist(
+    flatten([
+      for key, path in local.manifests : [
+        for id in data.kustomization_overlay.istio_configuration[key].ids : {
+          id       = id
+          manifest = data.kustomization_overlay.istio_configuration[key].manifests[id]
+        }
+      ]
+  ]))
 
   manifests = {
     authn = "${path.module}/istio-configuration/authorization-policy.yaml"
