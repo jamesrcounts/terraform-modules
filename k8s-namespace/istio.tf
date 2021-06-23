22634e3a7994ae5b73c148348acf729e679d032d
Jim Counts <jamesrcounts@outlook.com>
2021-06-23 14:29:25 +0000

    switch to kustomization overlay

--- k8s-namespace/istio.tf
+++ k8s-namespace/istio.tf
@@ -1,22 +1,28 @@
-resource "local_file" "policy" {
-  content = templatefile(
-    "${path.module}/istio-configuration/authorization-policy.yaml.hcl",
-    {
-      namespace = kubernetes_namespace.ns.metadata.0.name
-    }
-  )
-  filename = "${path.module}/istio-configuration/authorization-policy.yaml"
-}
+// resource "local_file" "policy" {
+//   content = templatefile(
+//     "${path.module}/istio-configuration/authorization-policy.yaml.hcl",
+//     {
+//       namespace = kubernetes_namespace.ns.metadata.0.name
+//     }
+//   )
+//   filename = "${path.module}/istio-configuration/authorization-policy.yaml"
+// }
 
-data "kustomization_build" "istio_configuration" {
-  depends_on = [local_file.policy]
+// data "kustomization_build" "istio_configuration" {
+//   path = "${path.module}/istio-configuration"
+// }
 
-  path = "${path.module}/istio-configuration"
+data "kustomization_overlay" "istio_configuration" {
+  namespace = kubernetes_namespace.ns.metadata.0.name
+
+  resources = [
+    "${path.module}/istio-configuration/authorization-policy.yaml"
+  ]
 }
 
 resource "kustomization_resource" "istio_configuration" {
+  for_each   = data.kustomization_overlay.istio_configuration.ids
   depends_on = [kubernetes_namespace.ns]
 
-  for_each = data.kustomization_build.istio_configuration.ids
-  manifest = data.kustomization_build.istio_configuration.manifests[each.value]
+  manifest = data.kustomization_overlay.istio_configuration.manifests[each.value]
 }
