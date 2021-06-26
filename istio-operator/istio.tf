d659b8576a013da7daeb98781bc67b3861602202
Jim Counts <jamesrcounts@outlook.com>
2021-06-26 14:54:33 +0000

    Cleanup istio module

--- istio-operator/istio.tf
+++
@@ -1,9 +0,0 @@
-data "kustomization" "istio" {
-  path = "${path.module}/istio"
-}
-
-resource "kustomization_resource" "istio" {
-  for_each   = data.kustomization.istio.ids
-  manifest   = data.kustomization.istio.manifests[each.value]
-  depends_on = [kustomization_resource.istio_operator, kubernetes_namespace.istio_system]
-}
--- istio-operator/operator.tf
+++ istio-operator/operator.tf
@@ -1,8 +1,8 @@
-data "kustomization" "istio_operator" {
+data "kustomization_build" "istio_operator" {
   path = "${path.module}/istio-operator"
 }
 
 resource "kustomization_resource" "istio_operator" {
-  for_each = data.kustomization.istio_operator.ids
-  manifest = data.kustomization.istio_operator.manifests[each.value]
+  for_each = data.kustomization_build.istio_operator.ids
+  manifest = data.kustomization_build.istio_operator.manifests[each.value]
 }
