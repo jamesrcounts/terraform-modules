ce2fa3888df0a8a4801ddda2981b7940175521cc
Jim Counts <jamesrcounts@outlook.com>
2021-10-07 14:27:57 +0000

    WIP Add Kubernetes

---
+++ fluent-bit/example/kubernetes/main.tf
@@ -0,0 +1,50 @@
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "aks-${local.instance_id}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "aks-${local.instance_id}"
+  kubernetes_version = data.azurerm_kubernetes_service_versions.current.latest_version
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = azurerm_resource_group.main.tags
+}
+
+data "azurerm_kubernetes_service_versions" "current" {
+  location = azurerm_resource_group.main.location
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-backend-${local.instance_id}"
+  location = "centralus"
+  tags = {
+    "instance_id" = local.instance_id
+  }
+}
+
+resource "random_pet" "instance_id" {}
+
+locals {
+    instance_id = random_pet.instance_id.id
+}
+
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+          source = "hashicorp/azurerm"
+          version = "2.78.0"
+      }
+  } 
+}
