022af2399f5859457c9ccec34aa948f0b14b1152
Jim Counts <jamesrcounts@outlook.com>
2021-12-30 16:05:02 +0000

    Default to AzureDiagnostics

--- diagnostics/diagnostics.tf
+++ diagnostics/diagnostics.tf
@@ -14,7 +14,7 @@ resource "azurerm_monitor_diagnostic_setting" "setting" {
   name                           = "diag-${each.key}"
   target_resource_id             = each.value
   log_analytics_workspace_id     = var.log_analytics_workspace_id
-  log_analytics_destination_type = contains(local.dedicated_tables, each.key) ? "Dedicated" : null
+  log_analytics_destination_type = contains(local.dedicated_tables, each.key) ? "AzureDiagnostics" : null
 
   dynamic "log" {
     for_each = data.azurerm_monitor_diagnostic_categories.categories[each.key].logs
